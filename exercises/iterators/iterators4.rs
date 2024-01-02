43fd218fc4bb4e60a0fcd08e75db098f8075783d
lucienvb <lucienvanbussel@gmail.com>
2024-01-02 18:09:29 +0100

    finished iterators

---
+++ exercises/iterators/iterators4.rs
@@ -0,0 +1,38 @@
+// iterators4.rs
+// Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
+
+pub fn factorial(num: u64) -> u64 {
+
+    // if num == 0 || num == 1 {
+    //     1
+    // }
+    // else {
+    //     num * factorial(num - 1)
+    // }
+
+    (1..=num).product() // without recursion
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(1, factorial(0));
+    }
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(1, factorial(1));
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(2, factorial(2));
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(24, factorial(4));
+    }
+}
